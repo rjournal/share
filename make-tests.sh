941d8100fb7f9f4ef3702d39e5310e47358b76c2
hadley <h.wickham@gmail.com>
2013-05-17 16:44:17 -0500

    Actually strip metadata

--- make-tests.sh
+++ make-tests.sh
@@ -11,10 +11,8 @@ rm -f *.png
 
 latexmk -pdf -quiet *.tex
 
-convert test-text.pdf -alpha remove test-test%02d.png
-convert test-misc.pdf -alpha remove test-misc%02d.png
-convert test-many-authors.pdf -alpha remove test-many-authors%02d.png
-
-pngcrush -ow -q *.png
+convert test-text.pdf -strip -alpha remove test-test%02d.png
+convert test-misc.pdf -strip -alpha remove test-misc%02d.png
+convert test-many-authors.pdf -strip -alpha remove test-many-authors%02d.png
 
 rm -f *.{aux,fls,.fdb_latexmk}
