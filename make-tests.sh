3f193e58f5302814c29973dd7368441231b9676f
hadley <h.wickham@gmail.com>
2013-05-17 16:34:12 -0500

    Use latexmk for building and crush pngs.

--- make-tests.sh
+++ make-tests.sh
@@ -7,13 +7,14 @@ latex RJournal.ins
 # Build all tests
 
 cd tests/
-pdflatex test-text.tex
+rm -f *.png
+
+latexmk -pdf -quiet *.tex
+
 convert test-text.pdf -alpha remove test-test%02d.png
-
-pdflatex test-misc.tex
 convert test-misc.pdf -alpha remove test-misc%02d.png
-
-pdflatex test-many-authors.tex
 convert test-many-authors.pdf -alpha remove test-many-authors%02d.png
 
-rm *.{aux}
+pngcrush -ow -q *.png
+
+rm -f *.{aux,fls,.fdb_latexmk}
