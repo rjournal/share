2ccac5a76497ac11c42b4db479de9b72a0365656
hadley <h.wickham@gmail.com>
2013-05-17 15:22:42 -0500

    Script to built test files. Low-res thumbnails will hopefully help spot regressions.

---
+++ make-tests.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+# Update style file
+latex RJournal.ins
+
+# Build all tests
+
+cd tests/
+pdflatex test-text.tex
+convert test-text.pdf -alpha remove test-test%02d.png
+
+pdflatex test-misc.tex
+convert test-misc.pdf -alpha remove test-misc%02d.png
+
+pdflatex test-many-authors.tex
+convert test-many-authors.pdf -alpha remove test-many-authors%02d.png
+
+rm *.{aux,syntex.gz}
