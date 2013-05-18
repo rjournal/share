1b7de587be9998f452df2650038a9ac7285824de
hadley <h.wickham@gmail.com>
2013-05-18 13:33:04 -0500

    Text formatting test

--- make-tests.sh
+++ make-tests.sh
@@ -12,6 +12,7 @@ rm -f *.png
 latexmk -pdf -quiet *.tex
 
 convert test-text.pdf -strip -alpha remove test-test%02d.png
+convert test-formatting.pdf -strip -alpha remove test-formatting%02d.png
 convert test-images.pdf -strip -alpha remove test-images%02d.png
 convert test-misc.pdf -strip -alpha remove test-misc%02d.png
 convert test-many-authors.pdf -strip -alpha remove test-many-authors%02d.png
